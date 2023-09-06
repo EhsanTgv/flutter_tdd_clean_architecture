4391eedd06432de7a443ddc06364aadbda290e76
EhsanTgv <EhsanTgv@gmail.com>
2023-09-06 10:54:36 +0300

    Implement Entities & Use Cases

---
+++ lib/core/error/failures.dart
@@ -0,0 +1,8 @@
+import 'package:equatable/equatable.dart';
+
+abstract class Failure extends Equatable {
+  const Failure([List<Object?> properties = const <Object?>[]]) : super();
+
+  @override
+  List<Object?> get props => [];
+}
