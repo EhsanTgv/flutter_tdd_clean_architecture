fc1a5d8aaf3eedf0a405547e5a5c1d0fa9ed180d
EhsanTgv <EhsanTgv@gmail.com>
2023-10-11 13:42:12 +0300

    Add Number Trivia to usecase

--- lib/features/number_trivia/domain/usecases/get_concrete_number_trivia.dart
+++ lib/features/number_trivia/domain/usecases/get_concrete_number_trivia.dart
@@ -1,16 +1,26 @@
 import 'package:dartz/dartz.dart';
+import 'package:equatable/equatable.dart';
 import 'package:flutter_tdd_clean_architecture/core/error/failures.dart';
+import 'package:flutter_tdd_clean_architecture/core/usecases/usecase.dart';
 import 'package:flutter_tdd_clean_architecture/features/number_trivia/domain/entities/number_trivia.dart';
 import 'package:flutter_tdd_clean_architecture/features/number_trivia/domain/repositories/number_trivia_repository.dart';
 
-class GetConcreteNumberTrivia {
+class GetConcreteNumberTrivia implements UseCase<NumberTrivia, int> {
   final NumberTriviaRepository repository;
 
   GetConcreteNumberTrivia(this.repository);
 
-  Future<Either<Failure, NumberTrivia>> call({
-    required int number,
-  }) async {
+  @override
+  Future<Either<Failure, NumberTrivia>> call(int number) async {
     return await repository.getConcreteNumberTrivia(number);
   }
+}
+
+class Params extends Equatable {
+  final int number;
+
+  Params({required this.number});
+
+  @override
+  List<Object> get props => [number];
 }
